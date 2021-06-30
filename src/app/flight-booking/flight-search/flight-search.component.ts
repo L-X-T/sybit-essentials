50fdce47d3b3c751264fddc1bd61f20021bc02c5
Alex Thalhammer <alex@angular-software.at>
2021-06-30 08:42:46 +0200

    1st Angular feature Module

--- src/app/flight-booking/flight-search/flight-search.component.ts
+++ src/app/flight-booking/flight-search/flight-search.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy } from '@angular/core';
 
-import { Flight } from '../entities/flight';
+import { Flight } from '../../entities/flight';
 import { FlightService } from './flight.service';
 import { Observable, Observer, Subject, Subscription } from 'rxjs';
 import { share, takeUntil } from 'rxjs/operators';
